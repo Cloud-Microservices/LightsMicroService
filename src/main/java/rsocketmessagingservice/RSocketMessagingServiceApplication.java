12b20a4b7d7eb02d4902ad1e0981c259a7ddb1a2
netanel97 <netanelhabas@gmail.com>
2024-02-12 11:20:46 +0200

    Init

---
+++ src/main/java/rsocketmessagingservice/RSocketMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package rsocketmessagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RSocketMessagingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RSocketMessagingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rsocketmessagingservice/RSocketMessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rsocketmessagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RSocketMessagingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
