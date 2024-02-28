311f3dc1126ca4833f9d0aeeeb783a47d13266ca
Omer1004 <omeriny@gmail.com>
2024-02-28 12:51:38 +0200

    RestLightsController done for http requests

--- src/main/java/lightsMicroService/controllers/RestLightsController.java
+++ src/main/java/lightsMicroService/controllers/RestLightsController.java
@@ -1,93 +1,84 @@
-//package lightsMicroService.controllers;
-//import lightsMicroService.boundaries.LightBoundary;
-//import lightsMicroService.logic.LightsService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//import reactor.core.publisher.Flux;
-//import reactor.core.publisher.Mono;
-//
-//@RestController
-//@RequestMapping("/lights")
-//public class RestLightsController {
-//
-//
-//    private LightsService lightsService;
-//
-//    @Autowired
-//    public RestLightsController(LightsService lightsService) {
-//        this.lightsService = lightsService;
-//    }
-//
-//    // Create light
-//    @PostMapping
-//    public Mono<LightBoundary> createLight(@RequestBody LightBoundary lightBoundary) {
-//        return lightsService.createLight(lightBoundary);
-//    }
-//
-//    // Delete light
-//    @DeleteMapping("/{id}")
-//    public Mono<Void> deleteLight(@PathVariable String id) {
-//        return lightsService.deleteLight(id);
-//    }
-//
-//    // Delete all lights
-//    @DeleteMapping
-//    public Mono<Void> deleteAllLights() {
-//        return lightsService.deleteAllLights();
-//    }
-//
-//    // Update light
-//    @PutMapping
-//    public Flux<LightBoundary> updateLight(@RequestBody Flux<LightBoundary> lightBoundaries) {
-//        return lightsService.updateLight(lightBoundaries);
-//    }
-//
-//    // Update specific light status
-//    @PutMapping("/status/{id}")
-//    public Mono<LightStatusBoundary> updateSpecificLightStatus(@PathVariable String id, @RequestBody LightStatusBoundary lightStatusBoundary) {
-//        return lightsService.updateSpecificLightStatus(id, lightStatusBoundary);
-//    }
-//
-//    // Update lights status in location
-//    @PutMapping("/status/location/{location}")
-//    public Flux<LightStatusBoundary> updateLightStatusByLocation(@PathVariable String location, @RequestBody LocationStatusBoundary locationStatusBoundary) {
-//        return lightsService.updateLightStatusByLocation(location, locationStatusBoundary);
-//    }
-//
-//    // Update All lights status
-//    @PutMapping("/status")
-//    public Flux<LightStatusBoundary> updateAllLightsStatus(@RequestBody StatusBoundary statusBoundary) {
-//        return lightsService.updateAllLightsStatus(statusBoundary);
-//    }
-//
-//    // Get light by ID
-//    @GetMapping("/{id}")
-//    public Mono<LightBoundary> getLightById(@PathVariable String id) {
-//        return lightsService.getLightById(id);
-//    }
-//
-//    // Get all lights
-//    @GetMapping
-//    public Flux<LightBoundary> getAllLights() {
-//        return lightsService.getAllLights();
-//    }
-//
-//    // Get all lights status
-//    @GetMapping("/status")
-//    public Flux<LightStatusBoundary> getAllLightsStatus() {
-//        return lightsService.getAllLightsStatus();
-//    }
-//
-//    // Get lights status in a specific location
-//    @GetMapping("/status/location/{location}")
-//    public Flux<LightStatusBoundary> getLightsStatusByLocation(@PathVariable String location) {
-//        return lightsService.getLightsStatusByLocation(location);
-//    }
-//
-//    // Get specific light status
-//    @GetMapping("/status/{id}")
-//    public Mono<LightStatusBoundary> getSpecificLightStatus(@PathVariable String id) {
-//        return lightsService.getSpecificLightStatus(id);
-//    }
-//}
-//
+package lightsMicroService.controllers;
+import lightsMicroService.boundaries.LightBoundary;
+import lightsMicroService.boundaries.LightStatusBoundary;
+import lightsMicroService.logic.LightsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/lights")
+public class RestLightsController {
+
+    private final LightsService lightsService;
+
+    @Autowired
+    public RestLightsController(LightsService lightsService) {
+        this.lightsService = lightsService;
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<LightBoundary> createLight(@RequestBody LightBoundary light) {
+        return lightsService.create(light);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteLight(@PathVariable String id) {
+        return lightsService.deleteLight(id);
+    }
+
+    @DeleteMapping
+    public Mono<Void> deleteAllLights() {
+        return lightsService.deleteAll();
+    }
+
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<LightBoundary> updateLight(@RequestBody LightBoundary light) {
+        return lightsService.updateLight(light);
+    }
+
+    @PutMapping(
+            path = "/status/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<LightStatusBoundary> updateSpecificLightStatus(@PathVariable String id, @RequestBody LightStatusBoundary status) {
+        return lightsService.updateLightStatus(id, status);
+    }
+
+    @PutMapping(path = "/status/location/{location}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<LightStatusBoundary> updateLightsStatusByLocation(@PathVariable String location, @RequestBody LocationStatusBoundary locationStatus) {
+        return lightsService.updateLightsStatusByLocation(location, locationStatus);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<LightBoundary> getLightById(@PathVariable String id) {
+        return lightsService.getLightById(id);
+    }
+
+    @GetMapping
+    public Flux<LightBoundary> getAllLights() {
+        return lightsService.getAllLights();
+    }
+
+    @GetMapping("/status")
+    public Flux<LightStatusBoundary> getAllLightsStatus() {
+        return lightsService.getAllLightsStatus();
+    }
+
+    @GetMapping("/status/location/{location}")
+    public Flux<LightStatusBoundary> getAllLightsStatusByLocation(@PathVariable String location) {
+        return lightsService.getAllLightsStatusByLocation(location);
+    }
+
+    @GetMapping("/status/{id}")
+    public Mono<LightStatusBoundary> getSpecificLightStatus(@PathVariable String id) {
+        return lightsService.getSpecificLightStatus(id);
+    }
+}
+
